e6e4f17c41beef1df8da90c14461ad20648d79a4
codajoker <84983434+codajoker@users.noreply.github.com>
2022-07-07 00:32:10 +0200

    Based

--- src/index.js
+++ src/index.js
@@ -1,19 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import { Provider } from 'react-redux';
-import { store, persistor } from './redux/store';
-import { BrowserRouter } from 'react-router-dom';
-import { PersistGate } from 'redux-persist/integration/react';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'react-toastify/dist/ReactToastify.css';
-ReactDOM.render(
+import React from "react";
+import App from "./App";
+import { Provider } from "react-redux";
+import { store,  } from "./redux/store";
+import { BrowserRouter } from "react-router-dom";
+import { createRoot } from "react-dom/client";
+const container = document.getElementById("root");
+const root = createRoot(container); // createRoot(container!) if you use TypeScript
+root.render(
   <BrowserRouter basename="/goit-react-hw-08-phonebook/">
     <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
-        <App />
-      </PersistGate>
+      {/* <PersistGate loading={null} persistor={persistor}> */}
+      <App />
+      {/* </PersistGate> */}
     </Provider>
-  </BrowserRouter>,
-  document.getElementById('root')
+  </BrowserRouter>
 );
