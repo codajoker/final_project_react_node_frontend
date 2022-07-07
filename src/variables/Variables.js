6073b39e5010ba1cfc974d43e62c7d6b99e70073
Zolotarova-Nataliia <zolotarova.nataliia@gmail.com>
2022-07-07 19:08:57 +0300

    [feat] Add main variables for styled-components

---
+++ src/variables/Variables.js
@@ -0,0 +1,20 @@
+// Color variables
+
+export const MAIN_COLOR = '#212121';
+export const SECONDARY_COLOR = '#9B9FAA';
+export const TEXT_COLOR = '#FFFFFF';
+export const BACKGROUND_COLOR = '#E5E5E5';
+export const SIDEBAR_COLOR = '#F0F1F3';
+export const SIDEBAR_TEXT_COLOR = '#9B9FAA';
+export const BUTTON_COLOR = '#FC842D';
+export const LINE_COLOR = '#E0E0E0';
+
+// Text variables
+
+export const TITLE_FONTSIZE_MOB = '18px';
+export const TITLE_FONTSIZE = '34px';
+
+export const SUBTITLE_FONTSIZE_MOB = '18px';
+export const SUBTITLE_FONTSIZE_DESC = '26px';
+
+export const TEXT_FONTSIZE = '14px';
