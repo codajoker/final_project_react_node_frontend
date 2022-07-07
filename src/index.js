37cbac69c1ee75a6de59bc2dc38fe8d1a495befb
codajoker <84983434+codajoker@users.noreply.github.com>
2022-07-07 19:22:06 +0200

    based

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import App from "./App.jsx";
 import { Provider } from "react-redux";
 import { store,  } from "./redux/store";
-import { BrowserRouter } from "react-router-dom";
+// import { BrowserRouter } from "react-router-dom";
 import ReactDOM from 'react-dom';
 
 ReactDOM.render(
