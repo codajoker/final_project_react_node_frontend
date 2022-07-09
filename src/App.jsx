83155d5840c684b66617b11e01bd81a5bdd34ec9
dmitruz <dmitruz970@gmail.com>
2022-07-09 18:59:35 +0300

    made UserInfo

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 import './App.css';
 import { Route, Routes } from 'react-router-dom';
 import Header from './components/Header/Header';
+import UserInfo from './components/UserInfo/UserInfo';
 
 const App = () => {
   return (
     <>
       <Header />
+      <UserInfo />
       <Routes>
         <Route path="/" />
         <Route path="/signin" />
