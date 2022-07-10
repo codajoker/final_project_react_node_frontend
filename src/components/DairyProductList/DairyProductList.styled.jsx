0e0528b504681914e0a1be527525a6904da9a6de
Bogdan <84569815+ortbogdan@users.noreply.github.com>
2022-07-10 22:08:00 +0300

    [styles] Add styles to the DairyProductList component
    - add transparency to last element in DairyProductList component

--- src/components/DairyProductList/DairyProductList.styled.jsx
+++ src/components/DairyProductList/DairyProductList.styled.jsx
@@ -1,10 +1,24 @@
 import styled from "styled-components";
 import { SIDEBAR_COLOR } from "../../variables/Variables";
-
+export const ProductsListThumb = styled.div`
+ position: relative;
+ @media screen and (min-width: 768px) {
+    :before{
+    width: 634px; 
+    position: absolute;
+    bottom: 0;
+    height: 50px;
+    left: 0;
+    right: 0;
+    background: linear-gradient(0, #fff, transparent);
+    content: "";
+  }}
+  `
 export const ProductsList = styled.ul`
     display: flex;
     flex-direction: column;
     align-items: center;
+   
     
 @media screen and (min-width: 768px) {
     overflow-y: scroll;
