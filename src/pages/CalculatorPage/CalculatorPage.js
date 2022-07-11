b45a4ff1ce69cf256355991cdd8df44eb2774659
albert95gt <albitakacs@gmail.com>
2022-07-11 14:51:10 +0200

    fix import in Calculator Page

--- src/pages/CalculatorPage/CalculatorPage.js
+++ src/pages/CalculatorPage/CalculatorPage.js
@@ -3,16 +3,14 @@ import DailyCaloriesForm from '../../components/DailyCaloriesForm/DailyCaloriesF
 import Header from '../../components/Header/Header';
 import RightSidebar from '../../components/RightSidebar/RightSidebar';
 import { SidebarWrapper, UserInfoContainer } from './CalculatorPage.styled';
-import UserInfo from '../../components/UserInfo/UserInfo';
+// import UserInfo from '../../components/UserInfo/UserInfo';
 
 const CalculatorPage = () => {
   return (
     <Fragment>
       <Header />
 
-      <UserInfoContainer>
-        <UserInfo />
-      </UserInfoContainer>
+      <UserInfoContainer>{/* <UserInfo /> */}</UserInfoContainer>
 
       <DailyCaloriesForm />
 
