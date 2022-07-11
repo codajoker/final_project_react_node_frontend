723469431ad088b30ca95893aa813e1023550785
dmitruz <dmitruz970@gmail.com>
2022-07-11 22:11:12 +0300

    userinfo dispatch

--- src/components/UserInfo/UserInfo.js
+++ src/components/UserInfo/UserInfo.js
@@ -1,15 +1,23 @@
 import React from 'react';
 import { HeaderDivider } from '../Header/Header.slyled';
 import {  UserInfoList, UserText,   UserExit } from "./UserInfo.styled";
+import { authOperations } from"../../redux/auth/authOperations";
+import { useDispatch } from 'react-redux';
+import { name } from '../../redux/auth/authSlice';
+//import { Link, Navigate } from 'react-router-dom';
 
 
 const UserInfo = () => {
-   
+
+const dispatch = useDispatch(); 
+
     return (
         <UserInfoList>
-              <li><UserText>Імя</UserText></li> 
+              <li><UserText>{name}І'мя</UserText></li> 
               <HeaderDivider />
-              <li><UserExit>Вихід</UserExit></li> 
+              <li><UserExit onClick={() => dispatch(authOperations.logOut())}>Вихід</UserExit></li> 
             </UserInfoList>
           );
 }
+
+export default UserInfo;
