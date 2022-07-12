8fa72acc25f6c682fc255ace15b9417c24e1cb2f
dmitruz <dmitruz970@gmail.com>
2022-07-12 18:26:36 +0300

    user-info  pravki

--- src/components/UserInfo/UserInfo.js
+++ src/components/UserInfo/UserInfo.js
@@ -1,21 +1,27 @@
-import React from 'react';
+import React  from 'react';
 import { HeaderDivider } from '../Header/Header.slyled';
 import {  UserInfoList, UserText,   UserExit } from "./UserInfo.styled";
 import { authOperations } from"../../redux/auth/authOperations";
-import { useDispatch } from 'react-redux';
-import { name } from '../../redux/auth/authSlice';
-//import { Link, Navigate } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { authOperations } from "../../redux/auth/authOperations";
+import { NavLink } from 'react-router-dom';
+
 
 
 const UserInfo = () => {
 
+  const name = useSelector(state => state.auth.user.name);
+  const logout = useSelector(state => state.logOut);
 const dispatch = useDispatch(); 
 
-    return (
+
+   return (
         <UserInfoList>
               <li><UserText>{name}І'мя</UserText></li> 
               <HeaderDivider />
+              <NavLink to={'/'}>
               <li><UserExit onClick={() => dispatch(authOperations.logOut())}>Вихід</UserExit></li> 
+              </NavLink>
             </UserInfoList>
           );
 }
