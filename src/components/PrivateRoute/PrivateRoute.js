77735953f2da51755575ed8952f62f03df664442
albert95gt <albitakacs@gmail.com>
2022-07-13 23:01:17 +0200

    fixed refreshing page

--- src/components/PrivateRoute/PrivateRoute.js
+++ src/components/PrivateRoute/PrivateRoute.js
@@ -2,7 +2,7 @@ import { useSelector } from 'react-redux';
 import { Navigate } from 'react-router-dom';
 import { getIsLoggedIn } from '../../redux/auth/authSelectors';
 
-export default function PrivateRoute({ children, navigateTo = '/signin' }) {
+export default function PrivateRoute({ children, navigateTo }) {
   const isLoggedIn = useSelector(getIsLoggedIn);
   return isLoggedIn ? children : <Navigate to={navigateTo} />;
 }
--- src/redux/auth/authSelectors.js
+++ src/redux/auth/authSelectors.js
@@ -1,4 +1,5 @@
 export const getIsLoggedIn = state => state.auth.isLoggedIn;
 export const getIsRegister = state => state.auth.isRegister;
 export const getUserName = state => state.auth.user.name;
+export const getIsRefreshing = state => state.auth.isRefreshing;
 export const getCalories = state => state.auth.user.dailyCalories;
