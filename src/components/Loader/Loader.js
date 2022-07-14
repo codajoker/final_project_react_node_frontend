d47d18391aa75076016ea872a9784e37fd30e9ab
Zolotarova-Nataliia <zolotarova.nataliia@gmail.com>
2022-07-15 01:59:51 +0300

    [fix] Fix error responce

--- src/components/Loader/Loader.js
+++ src/components/Loader/Loader.js
@@ -1,14 +1,17 @@
+import { BUTTON_COLOR } from '../../variables/Variables';
+import { ThreeCircles } from 'react-loader-spinner';
+import { LoaderBox } from './Loader.styled';
 
-import { BUTTON_COLOR} from "../../variables/Variables"
-import { ThreeCircles } from "react-loader-spinner"
-import { LoaderBox } from "./Loader.styled"
-
-export const Loader = () => {
-    return <LoaderBox>
-    <ThreeCircles
-       color={BUTTON_COLOR}
-       outerCircleColor='#2AA955'
-       height={90}
-       width={90}
-       ariaLabel="three-circles-rotating"
-/></LoaderBox>}
+export default function Loader() {
+  return (
+    <LoaderBox>
+      <ThreeCircles
+        color={BUTTON_COLOR}
+        outerCircleColor="#2AA955"
+        height={90}
+        width={90}
+        ariaLabel="three-circles-rotating"
+      />
+    </LoaderBox>
+  );
+}
--- src/services/productsApi.js
+++ src/services/productsApi.js
@@ -4,43 +4,37 @@ import moment from 'moment';
 axios.defaults.baseURL = 'https://sheltered-fjord-67703.herokuapp.com/';
 
 export const addProduct = async product => {
-  try {
-    const { data } = await axios.post('/product/addDiaryFood', product);
-    return data;
-  } catch (error) {
-    console.log(error);
-  }
+  const { data } = await axios.post('/product/addDiaryFood', product);
+  return data;
 };
 
 export const deleteProduct = async (date, id) => {
-  try {
-    const formatedDate = moment(date).format('DD.MM.yyyy');
-    const deletedProduct = await axios.delete(
-      `/product/delDiaryFood/?day=${formatedDate}&id=${id}`
-    );
-    return deletedProduct;
-  } catch (error) {
-    console.log(error);
-  }
+  const formatedDate = moment(date).format('DD.MM.yyyy');
+  const deletedProduct = await axios.delete(
+    `/product/delDiaryFood/?day=${formatedDate}&id=${id}`
+  );
+  return deletedProduct;
 };
 
 export const getProductByQuery = async query => {
-  try {
-    const { data } = await axios.get(`/product/${query}`);
-    return data;
-  } catch (error) {
-    console.log(error);
-  }
+  const { data } = await axios.get(`/product/${query}`);
+  return data;
 };
 
 export const getProductsListByDate = async date => {
   try {
-
-    const { data } = await axios.post("/users/dayinfo", {
+    const { data } = await axios.post('/users/dayinfo', {
       day: moment(date).format('DD.MM.yyyy'),
     });
     return data;
   } catch (error) {
-    console.log(error);
+    if (error.response.status === 404) {
+      return {
+        data: {
+          foodList: [],
+        },
+      };
+    }
+    throw error;
   }
 };
