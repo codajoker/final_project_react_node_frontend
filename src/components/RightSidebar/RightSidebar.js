66673cf17e45920d391a9410644a137e37f36eea
Zolotarova-Nataliia <zolotarova.nataliia@gmail.com>
2022-07-15 03:40:20 +0300

    [feat] Add loader and fix page render
    - Add loader to DiaryPage
    - Fix size of product title
    - Remove unnecessary totalDayCalories from slice and add existingProduct checking
    - remove unnecessary selector

--- src/components/RightSidebar/RightSidebar.js
+++ src/components/RightSidebar/RightSidebar.js
@@ -1,6 +1,8 @@
 import { Fragment } from 'react';
 import { useSelector } from 'react-redux';
-// import { translate } from '../../helpers/translate';
+import { translate } from '../../helpers/translate';
+
+import product_dictionary from '../../product_dictionary';
 import {
   Container,
   Head,
@@ -12,13 +14,21 @@ import {
   getCalories,
   getProducts,
 } from '../../redux/dailyRate/dailyRateSelectors';
-import { getUserTotalCalories } from '../../redux/products/productsSelectors';
-
+import {
+  getIsLoading,
+  getProductsList,
+} from '../../redux/products/productsSelectors';
+import Loader from '../Loader/Loader';
 
 export default function RightSidebar({ date }) {
   const products = useSelector(getProducts) || [];
   const dailyNormCalories = useSelector(getCalories) || 0;
-  const totalDayCalories = useSelector(getUserTotalCalories);
+  const productsList = useSelector(getProductsList);
+  const isLoading = useSelector(getIsLoading);
+  const totalDayCalories = productsList.reduce(
+    (total, x) => total + x.calories_kcal,
+    0
+  );
   let left = dailyNormCalories - totalDayCalories;
   let leftCalories =
     left < 0
@@ -33,45 +43,53 @@ export default function RightSidebar({ date }) {
 
   return (
     <Container>
-      <div>
-        <Head>Всього за {date}</Head>
-        <CalloriesList>
-          <li>
-            <span>Залишилось</span>
-            <span>{dailyNormCalories === 0 ? 0 : leftCalories}</span>
-          </li>
-          <li>
-            <span>Вжито</span>
-            <span>{dailyNormCalories === 0 ? 0 : totalDayCalories} кКал</span>
-          </li>
-          <li>
-            <span>Добова норма</span>
-            <span>{dailyNormCalories} кКал</span>
-          </li>
-          <li>
-            <span>n% від норми</span>
-            <span>{norm_persent} %</span>
-          </li>
-        </CalloriesList>
-      </div>
-      <div>
-        <Head>Не рекомендовано вживати</Head>
-        {products.length > 0 ? (
-          <ProductsList>
-            {products.map((product, index) => {
-              return (
-                <Fragment key={index}>
-                  {product}
-                  {index !== products.length - 1 && ', '}
-                </Fragment>
-              );
-            })}
-          </ProductsList>
-        ) : (
-          <EmptyProducts>Тут відображатиметься ваша дієта</EmptyProducts>
-        )}
-      </div>
+      {isLoading ? (
+        <Loader />
+      ) : (
+        <>
+          <div>
+            <Head>Всього за {date}</Head>
+
+            <CalloriesList>
+              <li>
+                <span>Залишилось</span>
+                <span>{dailyNormCalories === 0 ? 0 : leftCalories}</span>
+              </li>
+              <li>
+                <span>Вжито</span>
+                <span>
+                  {dailyNormCalories === 0 ? 0 : totalDayCalories} кКал
+                </span>
+              </li>
+              <li>
+                <span>Добова норма</span>
+                <span>{dailyNormCalories} кКал</span>
+              </li>
+              <li>
+                <span>n% від норми</span>
+                <span>{norm_persent} %</span>
+              </li>
+            </CalloriesList>
+          </div>
+          <div>
+            <Head>Не рекомендовано вживати</Head>
+            {products.length > 0 ? (
+              <ProductsList>
+                {products.map((product, index) => {
+                  return (
+                    <Fragment key={index}>
+                      {translate(product_dictionary, product)}
+                      {index !== products.length - 1 && ', '}
+                    </Fragment>
+                  );
+                })}
+              </ProductsList>
+            ) : (
+              <EmptyProducts>Тут відображатиметься ваша дієта</EmptyProducts>
+            )}
+          </div>
+        </>
+      )}
     </Container>
   );
 }
-
