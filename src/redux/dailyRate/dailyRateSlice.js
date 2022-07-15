591feb55690e2a75e00978c1d647028476b19c21
albert95gt <albitakacs@gmail.com>
2022-07-15 15:01:56 +0200

    fixed dailyRateSlice

--- src/redux/dailyRate/dailyRateSlice.js
+++ src/redux/dailyRate/dailyRateSlice.js
@@ -1,6 +1,6 @@
 import { createSlice } from '@reduxjs/toolkit';
 import { fetchDailyRateAuthorized } from './dailyRateOperations';
-import { refresh } from '../auth/authOperations';
+import { refresh, logOut } from '../auth/authOperations';
 
 const initialState = {
   dailyCalories: null,
@@ -26,6 +26,10 @@ const dailyRateSlice = createSlice({
     [refresh.fulfilled]: (state, { payload }) => {
       state.dailyCalories = payload.data.dailyCalories;
       state.uniqCategories = payload.data.notAllowedFood;
+    },
+    [logOut.fulfilled]: state => {
+      state.dailyCalories = null;
+      state.uniqCategories = [];
     },
   },
 });
