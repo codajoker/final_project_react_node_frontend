75a2d6462d64820b61277d7f1e99952b0db2965a
Olha Zhokhova <zhokhova.olha@gmail.com>
2022-07-17 15:07:11 +0300

    fix mobile max height

--- src/components/DairyProductList/DairyProductList.styled.js
+++ src/components/DairyProductList/DairyProductList.styled.js
@@ -2,18 +2,22 @@ import styled from 'styled-components';
 import { SIDEBAR_COLOR } from '../../variables/Variables';
 export const ProductsListThumb = styled.div`
   position: relative;
-  @media screen and (min-width: 768px) {
-    max-height: 240px;
+  max-height: 240px;
+  :before {
+    position: absolute;
+    bottom: -2px;
+    height: 60px;
+    left: 0;
+    right: 6px;
+    background: linear-gradient(0, rgba(255,255,255,1), rgba(255,255,255,0));
+    pointer-events: none;
+    z-index: 1;
+    content: '';
+  }
+  @media screen and (max-width: 767px) {
     :before {
-      width: 634px;
-      position: absolute;
-      bottom: 0;
-      height: 60px;
-      left: 0;
       right: 0;
-      background: linear-gradient(0, rgba(255,255,255,1), rgba(255,255,255,0));
-      pointer-events: none;
-      content: '';
+      height: 40px;
     }
   }
 `;
@@ -21,13 +25,12 @@ export const ProductsList = styled.ul`
   display: flex;
   flex-direction: column;
   align-items: center;
-    max-height: 230px;
+  max-height: 240px;
+  overflow-y: auto;
+  align-items: flex-start;
 
   @media screen and (min-width: 768px) {
-    overflow-y: auto;
-   
     width: 640px;
-    align-items: flex-start;
     ::-webkit-scrollbar {
       width: 6px;
     }
@@ -37,7 +40,5 @@ export const ProductsList = styled.ul`
     ::-webkit-scrollbar-track {
       background: ${SIDEBAR_COLOR};
     }
-    
-  
   }
 `;
