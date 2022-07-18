478be3f67ffd0994a82522a11e653037e8e3604a
Olha Zhokhova <zhokhova.olha@gmail.com>
2022-07-18 22:45:59 +0300

    fix start loader

--- src/components/Loader/Loader.styled.js
+++ src/components/Loader/Loader.styled.js
@@ -5,4 +5,6 @@ export const LoaderBox = styled.div`
    justify-content: center;
    align-items: center;
    min-height: ${props => props.full ? "100vh" : "100%"};
+   position: relative;
+   background: ${props => props.full ? props => props.theme.body : null};
 `
